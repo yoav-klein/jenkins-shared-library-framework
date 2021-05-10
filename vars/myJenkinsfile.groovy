aacfa67a5beb19f6375d9ca9d4bcd98a403f68d4
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2021-05-10 18:34:18 +0300

    Update myJenkinsfile.groovy

--- vars/myJenkinsfile.groovy
+++ vars/myJenkinsfile.groovy
@@ -2,8 +2,8 @@
 def call(Map args)
 {
     //def custom_params = new ArrayList<ParameterDefinition>()
-    //custom_params << args.custom_params
-    def custom_params = []
+    
+    def custom_params = args.custom_params
     custom_params << string(name: 'Branch', description: 'This is a generic parameter', defaultValue: '')
     properties([parameters(custom_params)])
     pipeline
