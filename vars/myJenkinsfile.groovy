91187aebf59d4f622f798b20373823ff58c51da2
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2021-05-10 18:45:47 +0300

    Update myJenkinsfile.groovy

--- vars/myJenkinsfile.groovy
+++ vars/myJenkinsfile.groovy
@@ -1,4 +1,24 @@
 
+/************************************************
+*   myJenkinsfile.groovy
+*   ------------------------
+*   This file demonstrates the use of a whole pipeline 
+*   as a function in a shared library.
+*
+*   This is useful in order to create a template Jenkinsfile to reuse
+*   the same logic in many Jenkins jobs.
+*
+*   It can take parameters to enable customizing the behaviour
+*   of the pipeline. In this example, it takes a parameter 
+*   of 'job_specific_params' - an ArrayList of parameters defined
+*   in the calling job, which are appended to parameters defined in this
+*   function that are generic parameters that should be used by all 
+*   the pipelines that use this pipeline
+*
+*
+***************************************************/
+
+
 def call(Map args)
 {
     // take job_specific_params into pipeline_params, 
